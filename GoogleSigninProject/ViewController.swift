1aa83175e80012cd24586b08d4059bcf890548c3
yachimoto <yachimoto0607@gmail.com>
2019-08-07 10:37:58 +0800

    Firstcommit

---
+++ GoogleSigninProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GoogleSigninProject
+//
+//  Created by SeitoYachimoto on 2019/08/07.
+//  Copyright © 2019 谷地元　星斗. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
